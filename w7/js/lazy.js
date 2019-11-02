bb2ff1256ab3a5b75e585870820c9efff81a736c
jeepyne <jessie.pyne@gmail.com>
2019-11-02 02:39:01 -0400

    week 7

--- w7/js/lazy.js
+++ w7/js/lazy.js
@@ -1,27 +1,27 @@
 let imagesToLoad = document.querySelectorAll("img[data-src]");
 
-const loadImages = function(image) {
+const loadImages = (image) => {
     image.setAttribute ("src", image.getAttribute("data-src"));
-    image.onload = function() {
+    image.onload = () => {
         image.removeAttribute("data-src");
     };
 };
 
 if ("IntersectionObserver" in window)
-{const observer = new IntersectionObserver (function(items, observer) {
-    items.forEach(function(item) {
+{const observer = new IntersectionObserver ((items, observer)=> {
+    items.forEach((item)=> {
     if(item.isIntersecting){
         loadImages (item.target);
         observer.unobserve(item.target);
         }
-    }, imgOptions);
+    });
 });
 
-imagesToLoad.forEach (function(img) {
+imagesToLoad.forEach ((img)=> {
     observer.observe(img);
     });
 }
-    else {imagesToLoad.forEach(function(img) {
+    else {imagesToLoad.forEach((img)=> {
         loadImages(img);
     });
 }
