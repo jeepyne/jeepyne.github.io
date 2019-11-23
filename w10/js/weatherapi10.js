2cbd6614c6d68dc8ada8e4a27e033004a151a046
jeepyne <jessie.pyne@gmail.com>
2019-11-23 11:49:16 -0500

    w10

--- w10/js/weatherapi10.js
+++ w10/js/weatherapi10.js
@@ -1,29 +1,38 @@
 const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=4e16acb56e4905a6e4220f0853f8c0ca";
 
+
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     //console.log(jsObject);
+
+    var temp = parseFloat(jsObject.main.temp);
+    var speed = parseFloat (jsObject.wind.speed);
+    var chill = "NaN";
+      if (temp <= 50 && speed > 3){
+        chill= 32.74 + (0.6215 * temp)-(35.75 * Math.pow(speed, 0.16)) + (0.4275 + temp + Math.pow (speed, 0.16));
+     }
+
+
     document.getElementById('currently').textContent = jsObject.weather[0].description;
-    document.getElementById('current-temp').textContent = jsObject.main.temp;
+    document.getElementById('current-temp').textContent = temp;
     document.getElementById('humidity').textContent = jsObject.main.humidity;
-    document.getElementById('windspeed').textContent = jsObject.wind.speed;
-    
-    var temp = parseFloat(document.getElementsByClassName("high").innerhtml);
-    var speed = parseFloat (document.getElementsByClassName("speed").innerhtml);
-    var chill = windChill (temp, speed);
-    
+    document.getElementById('windspeed').textContent = speed;
     
     document.getElementsByClassName("windChill").innerhtml = chill;
     
-    function windChill (temp, speed) {
-        var f = "NaN";
-        if (temp <= 50 && speed > 3){
-            f= 32.74 + (0.6215 * temp)-(35.75 * Math.pow(speed, 0.16)) + (0.4275 + temp + Math.pow (speed, 0.16));
-        }
-        return f;
-    }
   });
+
+
+
+
+
+
+
+
+
+
+
 
   fetch(apiURL)
   .then((response) => response.json())
