e88545aaadacb3986f0e4efbd106e1abdef71cf1
jeepyne <jessie.pyne@gmail.com>
2019-11-26 14:51:40 -0500

    w10

--- w10/js/weatherapi10.js
+++ w10/js/weatherapi10.js
@@ -1,6 +1,5 @@
 const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=4e16acb56e4905a6e4220f0853f8c0ca";
 
-
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
@@ -11,9 +10,8 @@ fetch(apiURL)
     var chill = "NaN";
       if (temp <= 50 && speed > 3){
         chill= 32.74 + (0.6215 * temp)-(35.75 * Math.pow(speed, 0.16)) + (0.4275 + temp + Math.pow (speed, 0.16));
-        chill.toFixed(2);
+        chill = chill.toFixed(2);
      }
-
 
     document.getElementById('currently').textContent = jsObject.weather[0].description;
     document.getElementById('current-temp').textContent = temp;
@@ -23,3 +21,13 @@ fetch(apiURL)
     document.getElementById("windChill").innerHTML = chill;
     
   });
+
+  const apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=4e16acb56e4905a6e4220f0853f8c0ca";
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+      let head = document.getElementById('head');
+      let row = document.getElementById('row');
+
+    }
