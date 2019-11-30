ccca1ab51c14346a2de0c240f338b60dde42b24f
jeepyne <jessie.pyne@gmail.com>
2019-11-29 22:40:17 -0500

    pancakes

--- w11/js/banner.js
+++ w11/js/banner.js
@@ -1,10 +1,14 @@
-function pancakes () {
-    let d = date();
+function pancakesSat () {
+    let d = new Date();
     let names = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let dayname = names [d.getDay()];
 
     if (dayname == "Friday"){
-        document.getElementById("pancakes").innerHTML = "Friday is Pancakes in the Park! 9:00am at the city park pavilion."
+        document.getElementById("pancakes").innerHTML = "Saturday is Pancakes in the Park! 9:00am at the city park pavilion."
     }
-    else {document.getElementById("pancakes").style.display = "none";}
+    else {document.getElementById("pancakes").style.display = "none";
     }
+}
+
+
+pancakesSat ()
